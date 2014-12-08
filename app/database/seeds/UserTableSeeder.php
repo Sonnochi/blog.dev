518cd6b66a022d9de5ccad9e620309c6f6947f19
Sonnochi <joseefarmer@yahoo.com>
2014-12-08 11:52:07 -0600

    Pusing all the things

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -6,8 +6,8 @@ class UserTableSeeder extends Seeder
      {
         $user = new User();
         
-        $user->email = 'joseefarmer@yahoo.com';
-        $user->password = Hash::make('password123');
+        $user->email = $_ENV['DEFAULT_USER'];
+        $user->password = Hash::make($_ENV['DEFAULT_PASS']);
         
         $user->save();
      }

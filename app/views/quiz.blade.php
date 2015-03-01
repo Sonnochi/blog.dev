2fa69cb1d23077bd30874543232555dd3a6377ca
Josee LeChele Farmer <Jfarmer@josees-mbp.attlocal.net>
2015-02-28 21:39:44 -0600

    Starting to add questions to quiz.

--- app/views/quiz.blade.php
+++ app/views/quiz.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
 
-
+          <div class="jumbotron">
+              <div class="container">
+                <h1>Test Me</h1>
+                <p>This is a template for a simple marketing or informational website.</p>
+              </div>
+        </div>
+        
+        <div class="container">
+            <div id="quiz">
+                <div id="question1">
+                    <div class="question">
+                        Which is not a main file type that we use to make websites?
+                    </div>
+                    <input type="raido" name="question1" value="a"/>
+                </div>
+            </div>
+            
+        </div>
 
 @stop

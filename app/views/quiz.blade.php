90f5a20faf38daa0360b0c6b2fb877b71705e801
Josee LeChele Farmer <Jfarmer@Josees-MacBook-Pro.local>
2015-03-09 20:53:29 -0500

    Added new question

--- app/views/quiz.blade.php
+++ app/views/quiz.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.master')
 
 @section('content')
-
+<script >
+    if (jQuery) {
+        var 
+    };
+</script>
           <div class="jumbotron">
               <div class="container">
                 <h1>Test Me</h1>
-                <p>This is a template for a simple marketing or informational website.</p>
               </div>
         </div>
         
@@ -18,7 +21,37 @@
                     <input type="radio" name="question1" value="a"/>
                     <label>.html</label><br>
                     <input type="radio" name="question1" value="b"/>
-                    <label>.exe</label>
+                    <label>.exe</label><br>
+                    <input type="radio" name="question1" value="c"/>
+                    <label>.js</label><br>
+                    <input type="radio" name="question1" value="b"/>
+                    <label>.css</label><br>
+                </div><br>
+                <div id="question2">
+                    <div class="question">
+                        A JavaScript object is wrapped by what characters?
+                    </div>
+                    <input type="radio" name="question2" value="a"/>
+                    <label>[ ]</label><br>
+                    <input type="radio" name="question2" value="b"/>
+                    <label>; ;</label><br>
+                    <input type="radio" name="question2" value="c"/>
+                    <label>{ }</label><br>
+                    <input type="radio" name="question2" value="b"/>
+                    <label>( )</label><br>
+                </div><br>
+                <div id="question3">
+                    <div class="question">
+                        Moles are which of the following?
+                    </div>
+                    <input type="radio" name="question3" value="a"/>
+                    <label>Omniverous</label><br>
+                    <input type="radio" name="question3" value="b"/>
+                    <label>Adorable</label><br>
+                    <input type="radio" name="question3" value="c"/>
+                    <label>Whackable</label><br>
+                    <input type="radio" name="question3" value="b"/>
+                    <label>All the above</label><br>
                 </div>
             </div>
             
